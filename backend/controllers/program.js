ea3ed21b2e986316fe577045e01af3c03ef9224d
sarastray <sara.stray@online.no>
2025-11-14 14:45:54 +0100

    Added program for admin
    Backend:
    - added methods for updating and deleting program
    - added validation to program
    Frontend
    - adminDashboard.jsx: added section for program
    -programService.js: handle connection with backend
    -added translation for program
    -fixed styling

--- backend/controllers/program.js
+++ backend/controllers/program.js
@@ -37,6 +37,11 @@ export const getProgram = (req, res) => {
 //legg til program
 export const addProgram = (req, res) => {
   const { Navn, Punkter } = req.body;
+  const bruker = req.user;
+
+  if (!bruker || !bruker.ErSuperbruker) {
+    return res.status(401).json({ error: "Ikke admin" });
+  }
 
   // Validate input
   if (!Navn || !Punkter) {
@@ -52,3 +57,72 @@ export const addProgram = (req, res) => {
   });
 };
 
+// Redigere en testfest
+export const updateProgram = (req, res) => {
+  const programID = Number(req.params.ProgramID);
+  const { Navn , Punkter } = req.body;
+  const bruker = req.user;
+
+  // Validate ProgramID
+  if (isNaN(programID) || programID <= 0) {
+    return res.status(400).json({ error: "Ugyldig ProgramID" });
+  }
+
+  // Sjekk innlogging
+  if (!bruker || !bruker.ErSuperbruker) {
+    return res.status(401).json({ error: "Ikke innlogget eller admin" });
+  }
+
+  // Validate input
+  if (!Navn || !Punkter) {
+    return res.status(400).json({ error: "Navn og Punkter er påkrevd" });
+  }
+
+  const query = "UPDATE Program SET Navn = ?, Punkter = ? WHERE ProgramID = ?";
+  const params = [Navn, Punkter, programID];
+
+  db.query(query, params, (err, result) => {
+    if (err) {
+      console.error("Feil ved oppdatering:", err);
+      return res.status(500).json({ error: "Kunne ikke oppdatere program" });
+    }
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ 
+        error: "Program ikke funnet eller ingen tilgang" 
+      });
+    }
+
+    res.json({ message: "Program oppdatert!" });
+  });
+};
+
+// Slette et program (kun superbruker)
+export const deleteProgram = (req, res) => {
+  const bruker = req.user;
+  const programID = Number(req.params.ProgramID);
+
+  // Sjekk at programID er gyldig
+  if (isNaN(programID) || programID <= 0) {
+    return res.status(400).json({ error: "Ugyldig ProgramID" });
+  }
+
+  // Sjekk at bruker er innlogget
+  if (!bruker || !bruker.ErSuperbruker) {
+    return res.status(401).json({ error: "Ikke admin" });
+  }
+
+  // Slett programmet
+  db.query("DELETE FROM Program WHERE ProgramID = ?", [programID], (err, result) => {
+    if (err) {
+      console.error("Feil ved sletting:", err);
+      return res.status(500).json({ error: "Kunne ikke slette program" });
+    }
+
+    // Hvis program ikke ble funnet
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: "Program ikke funnet" });
+    }
+    res.json({ message: "Program slettet!" });
+  });
+};
